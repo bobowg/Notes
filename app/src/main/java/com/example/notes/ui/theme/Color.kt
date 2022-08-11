76862de6a90933ca0e8190df84bfb1e99aa93383
bobowg <bobowg@163.com>
2022-08-11 12:29:04 +0800

    look

---
+++ app/src/main/java/com/example/notes/ui/theme/Color.kt
@@ -0,0 +1,12 @@
+package com.example.notes.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val DarkGray = Color(0xFF202020)
+val LightBule = Color(0xFFD7E8DE)
+
+val RedOrange =  Color(0xFFFF9800)
+val RedPink =  Color(0xFFE91E63)
+val BabyBule =  Color(0xFF3F51B5)
+val Violet =  Color(0xFF673AB7)
+val LightGreen =  Color(0xFF4CAF50)
