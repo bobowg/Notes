f23aef3cc374ad521392c36e6ea38004a3a2440d
bobowg <bobowg@163.com>
2022-08-11 14:14:34 +0800

    look

--- app/src/main/java/com/example/notes/feature_notes/data/data_source/NoteDatabase.kt
+++ app/src/main/java/com/example/notes/feature_notes/data/data_source/NoteDatabase.kt
@@ -5,6 +5,10 @@ import androidx.room.RoomDatabase
 import com.example.notes.feature_notes.domain.model.Note
 
 @Database(entities = [Note::class], version = 1)
-abstract class NoteDatabase:RoomDatabase() {
-    abstract val noteDao:NoteDao
+abstract class NoteDatabase : RoomDatabase() {
+    abstract val noteDao: NoteDao
+
+    companion object {
+        const val DATABASE_NAME = "notes_db"
+    }
 }
